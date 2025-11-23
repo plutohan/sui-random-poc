149f9ee4a80d7567871568ec18f235ad7ecca1a1
pluto <rtigerhan@gmail.com>
2025-11-23 13:49:42 +0900

    fix build error

--- frontend/src/views/HomeView.tsx
+++ frontend/src/views/HomeView.tsx
@@ -1,14 +1,13 @@
-import { FC } from "react";
-import LotteryInteraction from "../components/lottery/LotteryInteraction";
-import LotteryGridList from "../components/lottery/components/LotteryGridList";
+import { FC } from "react"
+import LotteryGridList from "../components/lottery/components/LotteryGridList"
 
 const HomeView: FC = () => {
-  return (
-    <div className="space-y-10">
-      <LotteryGridList />
-      {/* <LotteryInteraction /> */}
-    </div>
-  );
-};
+	return (
+		<div className="space-y-10">
+			<LotteryGridList />
+			{/* <LotteryInteraction /> */}
+		</div>
+	)
+}
 
-export default HomeView;
+export default HomeView
